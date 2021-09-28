b3a80ae6f5ade7814f4d7325eb9a5e517da92c88
Cédrick <linkitok44@gmail.com>
2021-09-28 18:18:01 +0200

    Add files product.js

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ window.addEventListener('load', function () {
     load_furnitures();
 });
 
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
+console.log(id)
 
 // Rempli la div meubles avec les données de l'API furniture
 function load_furnitures() {
     let div = document.getElementById("meubles");
 
-    fetch("http://localhost:3000/api/furniture")
+    fetch(`http://localhost:3000/api/furniture/`)
         .then(Response => Response.json())
-        .then(Furniture => {
+        .then( Furniture => {
             for (let element of Furniture) {
                 
+               
                 let content = creatDiv(element)
                 div.innerHTML += content;
+                  
             }
         });
 }   
@@ -29,5 +34,10 @@ function creatDiv(element) {
                         <div class="name_oak">${element.name}</div>
                             <div class="price">${element.price/100}€</div>
                                 </div></div></a>`;
+}
+
+
+function creatId(sol){
+    return `<div class="furniture-oak">${element.id}</div`
 }
 
