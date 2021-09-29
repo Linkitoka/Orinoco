3146045842dd0a6ea0f257dbd2d67380d54aee75
Cédrick <linkitok44@gmail.com>
2021-09-29 19:40:46 +0200

    Add function ofr product

--- product.js
+++ product.js
@@ -2,7 +2,7 @@
 // S'execute au chargement de la page
 window.addEventListener('load', function () {
     // Injecte les données de l'API furniture
-    loadProduct();
+    load_furnitures();
 });
 
 const params = new URLSearchParams(window.location.search)
@@ -10,26 +10,25 @@ const id = params.get("id")
 console.log(id)
 
 // Rempli la div meubles avec les données de l'API furniture
-function loadProduct() {
-    let prod = document.getElementById("meubles");
+function load_furnitures() {
+    let div = document.getElementById("meubles");
 
     fetch(`http://localhost:3000/api/furniture/${id}`)
         .then(Response => Response.json())
         .then(Furniture => {
-            
-                
+                let content = creatDiv(Furniture)
+                div.innerHTML += content;
                
-                let write = creatProduct(element)
-                prod.innerHTML += write;
-                  
             }
-        });
+        );
+}   
 
-function creatProduct(element) {
-    return `<div class="furniture">
-                <div class="image"><img src="${element.imageUrl}"></img></div>
-                    <div class="pricename">
-                        <div class="nae">${element.name}</div>
-                            <div class="prie">${element.price/100}€</div>
-                                </div></div>`;
+// Retourne une div avec ses classes son contenu
+function creatDiv(element) {
+    return `<div class="furniture-oak">
+                <div class="image_oak"><img src="${element.imageUrl}"></img></div>
+                    <div class="price-name">
+                        <div class="name_oak">${element.name}</div>
+                            <div class="price">${element.price/100}€</div>
+                                </div></div></a>`;
 }
